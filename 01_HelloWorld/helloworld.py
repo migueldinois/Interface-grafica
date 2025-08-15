d6c3b3db82c699597a9999343e6ae3485d7255fc
migueldinois <Migueldinois10@gmail.com>
2025-08-15 14:55:28 -0300

    Colocado bom dia 3000

--- 01_HelloWorld/helloworld.py
+++ 01_HelloWorld/helloworld.py
@@ -2,19 +2,40 @@ import tkinter as tk
 
 #Criando janela e titulo.
 janela = tk.Tk()
-janela.title = ("Painel Profissional")
+janela.title("Spotify")
 
-#Aumentando a tela
-janela.geometry("1280x720+260+160")
+janela.geometry("1280x720+260+160")#Aumentando a tela
+janela.resizable(False, False) # Para nao deixar o usuario aumentar ou diminuir a tela
+janela.iconbitmap("01_HelloWorld/icons/spotify.ico")
+janela.configure(bg="#1DB954")#Mudar cor do fundo
 
-# Para nao deixar o usuario aumentar ou diminuir a tela
-janela.resizable(False, False)
+#Funcoes botoes
 
-#Mudar cor do fundo
-janela.configure(bg="#FF0E0F")
+def acao():
+    nome = campo_musica.get()
+    label_aviso.config(text=f"Bom dia, {nome}")
 
-label = tk.Label(janela, text="Hello World")
-label.pack() # Label pack é para pegar a label e colocar em um widget, tipo compactar em uma widget
+#texto  bom dia
+label_bomdia = tk.Label(janela, text="Digite seu nome e receba um bom dia do incrivel BOM DIA 3000!",
+                        bg="#49be25",
+                        font=("Verdana", 20), 
+                        )
+label_bomdia = label_bomdia.pack()
+# Textos aviso
+label_aviso = tk.Label(janela, bg="#43fb2d",
+                       font=("Arial", 19)
+)
+label_aviso.pack(pady=1, padx=300) # Label pack é para pegar a label e colocar em um widget, tipo compactar em uma widget
+
+
+# Campos de textos:
+
+campo_musica = tk.Entry(janela)
+campo_musica.pack(pady=5)
+
+#Botoes
+botao_procurar = tk.Button(janela, text="Procurar musica", command=acao)
+botao_procurar.pack()
 
 # Deixando janela em loop para nao fechar
 janela.mainloop()
