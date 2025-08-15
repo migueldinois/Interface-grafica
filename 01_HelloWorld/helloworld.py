8bcb76919166301e7e34351b3c52077a069998c3
migueldinois <Migueldinois10@gmail.com>
2025-08-15 11:30:05 -0300

    aee

--- 01_HelloWorld/helloworld.py
+++ 01_HelloWorld/helloworld.py
@@ -4,6 +4,14 @@ import tkinter as tk
 janela = tk.Tk()
 janela.title = ("Painel Profissional")
 
+#Aumentando a tela
+janela.geometry("1280x720+260+160")
+
+# Para nao deixar o usuario aumentar ou diminuir a tela
+janela.resizable(False, False)
+
+#Mudar cor do fundo
+janela.configure(bg="#FF0E0F")
 
 label = tk.Label(janela, text="Hello World")
 label.pack() # Label pack é para pegar a label e colocar em um widget, tipo compactar em uma widget
