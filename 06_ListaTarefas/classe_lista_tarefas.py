ba058ad7a0612378c87c1048799e0726a10accda
migueldinois <miguel.dinois@aluno.senai.br>
2025-09-19 11:18:07 -0300

    Novo Projeto de Lista de tarefas, Tela de login finalizada, terminando agora a tela principal

---
+++ 06_ListaTarefas/classe_lista_tarefas.py
@@ -0,0 +1,77 @@
+
+import tkinter.messagebox
+import ttkbootstrap as ttk
+import tkinter
+import json
+import random
+from tkinter import messagebox
+
+
+class ListaDeTarefas:
+
+    
+
+    def __init__(self):
+        self.janela = ttk.Window(themename="vapor")
+        self.janela.iconbitmap("06_ListaTarefas/icons/icone.ico")
+        self.janela.geometry("1280x720+260+160")
+        self.janela.resizable(False, False)
+        self.janela.title("Lista de Tarefas")
+
+    # Titulo Principal da lista
+
+        titulo = ttk.Label(self.janela, text="Lista de Tarefas", font=("Verdana", 20), padding=5)
+        titulo.pack(pady=30)
+
+    #  Descricao do app
+        descricao = ttk.Label(self.janela, text="Seja organizado e utilize ese app para te ajudar no dia a dia!", font=("Verdana", 15), padding=10)
+        descricao.pack()
+
+    # Campo de escrever e adcionar tarefa 
+
+        self.tarefa = ttk.Entry(self.janela, width=100)
+        self.tarefa.pack(pady=40)
+    
+        logar_botao = ttk.Button(text="Adicionar Tarefa",padding=5, command=self.add_tarefa)
+        logar_botao.pack(pady=20)
+        logar_botao.place(x=535, y=230)
+
+        cancelar_botao = ttk.Button(text="Remover Tarefa", padding=5)
+        cancelar_botao.pack(pady=20)
+        cancelar_botao.place(x=650, y= 230) 
+
+    # Campo para listar as tarefas
+        self.lista_tarefas = ttk.Text(wrap="word", state="disabled")
+        self.lista_tarefas.pack()
+
+
+    # Funcao para adicionar tarefa a lista
+
+    def add_tarefa(self):
+        self.lista_tarefas.config(state="normal")
+        # Pegar o texto do entryu
+        self.tarefa_texto = self.tarefa.get()
+        # Salvar a lista inteira no dicionario
+        lista_de_tarefas = {"teste": self.tarefa_texto}
+        nome_arquivo_json = "dados_texto.json"
+        try:
+            with open(nome_arquivo_json, 'w') as arquivo_json:
+                json.dump(lista_de_tarefas, arquivo_json)
+        except:
+            messagebox.askretrycancel("Erro", "Não foi possivel salvar")
+
+
+        tkinter.messagebox.showinfo("Tarefa Adicionada", f"A tarefa {self.tarefa_texto} foi adicionada a lista de tarefas!")
+
+        self.lista_tarefas.insert(ttk.END, f"{self.tarefa_texto} \n")
+        self.lista_tarefas.config(state="disabled")
+
+    def run(self):
+        self.janela.mainloop()
+
+
+    
+
+if __name__ == "__main__":
+    app = ListaDeTarefas()
+    app.run()
---
+++ 06_ListaTarefas/classe_login.py
@@ -0,0 +1,69 @@
+import tkinter.messagebox
+import ttkbootstrap as ttk
+import tkinter
+import json
+import random
+from tkinter import messagebox
+
+class Login:
+    def __init__(self):
+        
+        self.janela = ttk.Window(themename="morph")
+        self.janela.protocol("WM_DELETE_WINDOW", self.quando_fechar)
+        self.janela.iconbitmap("06_ListaTarefas/icons/icone.ico")
+        self.janela.geometry("1280x720+260+160")
+        self.janela.resizable(False, False)
+        self.janela.title("Lista de Tarefas - Login")
+        
+
+        # Titulo Principal da janela de login
+        bemVindo = ttk.Label(self.janela, text="Bem vindo!", font=("Segoe UI", 20))
+        bemVindo.pack()
+
+        loginContinue = ttk.Label(self.janela, text="Faça login para continuar:", font=("Verdana", 14), padding=10)
+        loginContinue.pack()
+
+        # Campos para colocar login e senha
+
+        self.campo_usuario = ttk.Entry(self.janela)
+        self.campo_usuario.pack(padx=10, pady=10)
+
+        self.campo_senha = ttk.Entry(self.janela, show="*")                                                    
+        self.campo_senha.pack()
+
+        # Botoes de cancelar e logar
+
+        frame_botao = ttk.Frame()
+        frame_botao.pack()
+
+        logar_botao = ttk.Button(frame_botao,text="Logar",padding=10, command=self.logar_funcao).pack(side="left", padx=4, pady=10)
+
+
+
+        cancelar_botao = ttk.Button(frame_botao,text="Cancelar", padding=10, command=self.exit).pack(side="right", padx=4)
+ 
+        # cancelar_botao.place(x=635, y=180)
+        
+    def logar_funcao(self):
+        self.resposta_usuario = self.campo_usuario.get()
+        self.resposta_senha = self.campo_senha.get()
+
+        if self.resposta_usuario == "1" and self.resposta_senha == "1":
+            tkinter.messagebox.showinfo("Logado com sucesso!", "Aproveite o melhor aplicativo do mundo!")
+        else:
+            tkinter.messagebox.showerror("Aviso!", "Seu login não existe ou está errado!")  
+
+    def exit(self):
+        self.janela.destroy()
+        
+    def quando_fechar(self):
+        if messagebox.askyesno("Aviso", "Deseja fechar o Aplicativo?"):
+            self.janela.destroy() 
+
+    def run(self):
+        self.janela.mainloop()
+        
+
+if __name__ == "__main__":
+    app = Login()
+    app.run()
